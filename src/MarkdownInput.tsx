859973335f2b7c05e2e51c08ec33e40e7dd255df
Joyal-George-KJ <joyalgeorgekj@gmail.com>
2025-06-09 18:35:43 +0530

    Impliments Reflected

--- src/MarkdownInput.tsx
+++ src/MarkdownInput.tsx
@@ -376,7 +376,7 @@ export default function MarkdownInput({
                                 className="toolbar-btn"
                                 id={val}
                             >
-                                {markdownShortcuts[val].icon()}
+                                {MarkdownShortcutIcon[val].icon()}
                             </button>
                         ))}
                     </div>
@@ -385,16 +385,7 @@ export default function MarkdownInput({
                             onClick={() => setPreviewToggle(!previewToggle)}
                             className="toolbar-btn preview"
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="16"
-                                height="16"
-                                fill="currentColor"
-                                className="bi bi-play-fill"
-                                viewBox="0 0 16 16"
-                            >
-                                <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393" />
-                            </svg>
+                            {MarkdownShortcutIcon.preview.icon()}
                         </button>
                     </div>
                 </div>
